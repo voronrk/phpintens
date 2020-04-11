8c9424e0cb7db2d6507c030895a99f9aa219dc6d
voron <voron_76@mail.ru>
2020-04-12 01:24:53 +0300

    Day 1 with hometasks

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php 
+	$pageTitle="Главная страница";
+	include("./templates/_head.php"); 
+?>
+
+	<!-- white-plate -->
+	<div class="white-plate">
+		<div class="container-fluid">
+			<?php include("./templates/_header.php"); ?>
+			<div class="line-between"></div>
+			<!-- content block -->
+			<div class="row">
+				<!-- Left aside -->
+				<?php include("./templates/_aside.php"); ?>
+				<!-- // Left aside -->
+				<!-- Center Part -->
+				<div class="col-md-9 col-lg-10">
+					<div class="row">
+
+						<!-- Товар 1 -->
+						<?php 
+							for ($i=1; $i<=5; $i++) {
+								include("./templates/_product-item.php");
+							}
+						?>
+						<!-- // Товар 1 -->
+
+					</div>
+				</div>
+				<!-- // Center Part -->
+			</div>
+			<!-- content block -->
+		</div>
+	</div>
+	<!-- // white-plate -->
+	
+<?php include("./templates/_footer.php"); ?>
---
+++ login.php
@@ -0,0 +1,28 @@
+<?php 
+	$pageTitle="Страница входа";
+	include("./templates/_head.php"); 
+?>
+
+	<!-- white-plate -->
+	<div class="white-plate white-plate--login">
+		<div class="container-fluid">
+
+			<!-- header -->
+			<?php include("./templates/_header.php"); ?>
+			<!-- // header -->
+
+			<div class="line-between"></div>
+			<form action="admin.html">
+				<div class="form-group">
+					<input type="password" class="form-control" placeholder="Пароль">
+				</div>
+				<div class="form-group">
+					<button type="submit" class="btn btn-primary btn-block">Войти</button>
+				</div>
+			</form>
+			<p class="text-center"><a href="index.html" class="text-secondary">Вернуться назад</a></p>
+		</div>
+	</div>
+	<!-- // white-plate -->
+
+<?php include("./templates/_footer.php"); ?>
